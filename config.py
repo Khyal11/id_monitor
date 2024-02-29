fb943de7cfa5283a37646021c7e74bfe9837a174
Khyal Nayak <115208914+Khyal11@users.noreply.github.com>
2024-03-01 00:13:55 +0530

    config.py

--- config.py
+++ config.py
@@ -2,6 +2,16 @@ import os
 
 ADDED_USERS_FILE = "added_users.json"
 
-# API_ID = os.environ.get("API_ID")
-# API_HASH = os.environ.get("API_HASH")
-# BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+class Config(object):
+
+    # Get a bot token from botfather
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+
+    # Get from my.telegram.org (or @UseTGXBot)
+    API_ID = int(os.environ.get("API_ID", 12345))
+
+
+    # Get from my.telegram.org (or @UseTGXBot)
+    API_HASH = os.environ.get("API_HASH", "")
