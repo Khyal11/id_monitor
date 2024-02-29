2c73f3a3846e81973f5fccfeb22d6c9be13d054b
Khyal Nayak <115208914+Khyal11@users.noreply.github.com>
2024-02-29 22:18:55 +0530

    config.py

--- config.py
+++ config.py
@@ -5,9 +5,3 @@ ADDED_USERS_FILE = "added_users.json"
 # API_ID = os.environ.get("API_ID")
 # API_HASH = os.environ.get("API_HASH")
 # BOT_TOKEN = os.environ.get("BOT_TOKEN")
-#
-# # config.py
-
-API_ID = "28346852"
-API_HASH = "a370d8cc54c5113d311d99d847962f02"
-BOT_TOKEN = "6270738616:AAEz-0xD86vm1uYFv42bidqU8HOd69OpvPg"
