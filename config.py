755f34442b4c9ef135dd3d988797936d9bc6d72c
Khyal Nayak <115208914+Khyal11@users.noreply.github.com>
2024-03-01 00:14:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ ADDED_USERS_FILE = "added_users.json"
 class Config(object):
 
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 
     # Get from my.telegram.org (or @UseTGXBot)
-    API_ID = int(os.environ.get("API_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", ))
 
 
     # Get from my.telegram.org (or @UseTGXBot)
